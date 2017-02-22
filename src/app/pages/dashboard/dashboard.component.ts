fc894d5b919559bfaab7471f48934cecde0438f8
Valentin FRIES <vfries@reseau-ges.fr>
2017-02-22 14:51:12 +0100

    Issue #4
     - Dashboard front-end

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
 import {ShardService} from "../../services/shard/shard.service";
 
+declare const particlesJS: any;
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'dashboard.component.html',
@@ -18,5 +20,7 @@ export class DashboardComponent implements OnInit {
 		if(!this.shardService.get()) {
 			this.router.navigateByUrl('/');
 		}
+
+		particlesJS.load('particles-js', 'assets/js/particles.json');
 	}
 }
